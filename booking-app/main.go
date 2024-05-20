d31a9d6ae1f0119a4cf54a767ab6a5f48a544449
Aaron Wang <aarongwang2008@gmail.com>
2024-05-20 21:13:00 +0800

    completed loops section

--- booking-app/main.go
+++ booking-app/main.go
@@ -2,22 +2,50 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	var conferenceName string = "Go Conference"
-	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	const conferenceTickets uint = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var username string
-	var userTickets int
-	// ask user for their name
+	var bookings = []string{}
 
-	username = "Tom"
-	userTickets = 2
-	fmt.Printf("User %v has booked %v tickets\n", username, userTickets)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	for {
+		// ask user for their info
+		fmt.Println("Enter your first name:")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name:")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email:")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets:")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, firstName + " " + lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+		var firstNames = []string{}
+		for _, booking := range bookings {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+		fmt.Printf("These are all the bookings (first names): %v\n", firstNames)
+	}
 }
